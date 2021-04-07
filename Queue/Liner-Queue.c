909664acf61e524208286d9a9cb81d86c6a57ab7
Danmark_Jajang <54851789+Danmark-Jajang@users.noreply.github.com>
2021-04-07 15:36:07 +0900

    Liner-Queue(선형 큐)
    만들기 쉽지만 front와 rear가 증가만하여 공간문제가 발생하여 따로 앞으로 당기는 연산을 해 줘야한다는 불편함이 있음

---
+++ Queue/Liner-Queue.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_QUEUE_SIZE 5
+
+typedef int element;
+typedef struct _QueueType{
+    int front;
+    int rear;
+    element data[MAX_QUEUE_SIZE];
+}QueueType;
+
+void init_queue(QueueType *q){
+    q->front = -1;
+    q->rear = -1;
+}
+
+void print_queue(QueueType *q){
+    int i;
+    for(i=0;i<MAX_QUEUE_SIZE;i++){
+        if(i<=q->front || i>q->rear){
+            printf("   | ");
+        }
+        else{
+            printf("%d | ",q->data[i]);
+        }
+    }
+    printf("\n");
+}
+
+int is_full(QueueType *q){
+    if(q->rear==MAX_QUEUE_SIZE-1) return 1;
+    else return 0;
+}
+
+int is_empty(QueueType *q){
+    if(q->front==q->rear) return 1;
+    else return 0;
+}
+
+void enqueue(QueueType *q, int item){
+    if(is_full(q)){
+        printf("Queue Full Error Occur");
+        exit(1);
+    }
+    else{
+        q->data[++(q->rear)] = item;
+    }
+}
+
+int dequeue(QueueType *q){
+    if(is_empty(q)){
+        printf("Queue Empty Error Occur");
+        exit(1);
+    }
+    else{
+        return q->data[++(q->front)];
+    }
+}
+
+void main(){
+    int item;
+    QueueType q;
+    init_queue(&q);
+
+    enqueue(&q, 10);
+    print_queue(&q);
+    enqueue(&q, 20);
+    print_queue(&q);
+    enqueue(&q, 30);
+    print_queue(&q);
+
+    item = dequeue(&q);
+    print_queue(&q);
+    item = dequeue(&q);
+    print_queue(&q);
+    item = dequeue(&q);
+    print_queue(&q);
+}
